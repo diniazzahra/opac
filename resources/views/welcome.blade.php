eb569e02a7975ebcecef8c6a2cef04a86ae5fd59
Rez <moehamadrezha@gmail.com>
2019-12-17 17:44:41 +0700

    remove public in url
    menghilangkan public dari url, menambahkan sedikit pesan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,19 +81,10 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    OPAC
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <b>Coming Soon!</b><br>
+We're working hard to finish the development of this site. 
             </div>
         </div>
     </body>
